f5d00f0658dec6b5bfc2dbd71e44bdf3609e2190
Raraph84 <contact@raraph.fr>
2024-05-16 19:43:09 +0200

    Fix for API update

--- src/api.js
+++ src/api.js
@@ -15,7 +15,7 @@ export const getNode = async (id) => new Promise((resolve, reject) => {
 export const getNodeUptime = async (nodeId, since, unit) => new Promise((resolve, reject) => {
 
     fetch(HOST + "/nodes/" + nodeId + "/uptimes", { method: "GET" }).then(async (res) => {
-        if (res.ok) res.json().then((res) => resolve(res.uptime)).catch((error) => reject(error));
+        if (res.ok) res.json().then((res) => resolve(res.uptimes)).catch((error) => reject(error));
         else res.json().then((res) => reject(res.message)).catch((error) => reject(error));
     }).catch((error) => reject(error));
 });
